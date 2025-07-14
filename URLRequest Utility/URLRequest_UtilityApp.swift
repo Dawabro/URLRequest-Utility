b76a64ff177b00f2a47545b07255e328b8f475b7
David W. Brown <davidwbrown@hey.com>
2025-07-14 12:12:41 -0500

    Initial Commit

---
+++ URLRequest Utility/URLRequest_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLRequest_UtilityApp.swift
+//  URLRequest Utility
+//
+//  Created by David Brown on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct URLRequest_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
